a9416c0e6dc2e88392b0b1af12b65dbbb7a7d633
JITHIN VIJAYAN <jithinv028@gmail.com>
2022-08-16 17:13:12 +0530

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent} from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+import {ProjectComponent} from './pages/project/project.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'projects',component:ProjectComponent},
+  {path: 'skills',component:SkillsComponent},
+  {path: 'Experience',component:ExperienceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { HeadderComponent } from './sharepages/headder/headder.component';
+import { FooterComponent } from './sharepages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    ExperienceComponent,
+    HomeComponent,
+    ProjectComponent,
+    SkillsComponent,
+    HeadderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
